6792334a2206758bdc1c4d846ab7e6612318026f
Arun Sathia <arunmsn19@gmail.com>
2024-03-22 15:19:35 -0500

    Added Base in ViewViewModels

---
+++ HabitsReelPlus/HabitsReelPlus/Models/Titles/TitleMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitsReelPlus.Models.Titles
+{
+    public static class TitleMain
+    {
+        public static string loginTitle { get; set; } = "Login";
+        public static string FgtPwd { get; set; } = "Forgot Password";
+    }
+}
